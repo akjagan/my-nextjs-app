cb8c5e8f97004ae668d985f19faeeaf0a4919cc9
jagan guvi <akjagankumar@gmail.com>
2024-12-05 13:19:18 +0530

    job task fe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="justify-items-center items-center gap-16 grid grid-rows-[20px_1fr_20px] p-8 sm:p-20 pb-20 min-h-screen font-[var(--font-geist-sans)]">
+      {/* Main Content */}
+      <main className="flex flex-col items-center sm:items-start gap-8 row-start-2">
+        {/* Logo */}
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,7 +14,9 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+
+        {/* Instructions */}
+        <ol className="font-[var(--font-geist-mono)] text-center text-sm sm:text-left list-decimal list-inside">
           <li className="mb-2">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
@@ -23,9 +27,11 @@ export default function Home() {
           <li>Save and see your changes instantly.</li>
         </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        {/* Actions */}
+        <div className="flex sm:flex-row flex-col items-center gap-4">
+          {/* Deploy Now */}
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            className="flex justify-center items-center gap-2 bg-foreground hover:bg-gray-700 dark:hover:bg-gray-300 px-4 sm:px-5 border border-transparent border-solid rounded-full h-10 sm:h-12 text-background text-sm sm:text-base transition-colors"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -33,23 +39,28 @@ export default function Home() {
             <Image
               className="dark:invert"
               src="/vercel.svg"
-              alt="Vercel logomark"
+              alt="Vercel logo"
               width={20}
               height={20}
             />
-            Deploy now
+            Deploy Now
           </a>
+
+          {/* Read Documentation */}
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="flex justify-center items-center border-gray-300 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 px-4 sm:px-5 border hover:border-transparent border-solid rounded-full sm:min-w-44 h-10 sm:h-12 text-sm sm:text-base transition-colors"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read Our Docs
           </a>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+
+      {/* Footer */}
+      <footer className="flex flex-wrap justify-center items-center gap-6 row-start-3">
+        {/* Learn Link */}
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -57,7 +68,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <Image
-            aria-hidden
+            aria-hidden="true"
             src="/file.svg"
             alt="File icon"
             width={16}
@@ -65,6 +76,8 @@ export default function Home() {
           />
           Learn
         </a>
+
+        {/* Examples Link */}
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -72,7 +85,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <Image
-            aria-hidden
+            aria-hidden="true"
             src="/window.svg"
             alt="Window icon"
             width={16}
@@ -80,6 +93,8 @@ export default function Home() {
           />
           Examples
         </a>
+
+        {/* Go to Next.js */}
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -87,7 +102,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <Image
-            aria-hidden
+            aria-hidden="true"
             src="/globe.svg"
             alt="Globe icon"
             width={16}
